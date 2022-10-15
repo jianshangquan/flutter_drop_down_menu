910575bb18096030bad0cda4e137a2cfe751ac5f
jianshangquan <jianshangquan3@gmail.com>
2022-10-15 14:40:30 +0630

    migrate to package

--- package/test/flutter_drop_down_test.dart
+++ package/test/flutter_drop_down_test.dart
@@ -4,9 +4,5 @@ import 'package:flutter_drop_down/flutter_drop_down.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
   });
 }
